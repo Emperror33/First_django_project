68e5708716b655976a1e828516b3ce7f1bc3222c
Samrat <paudelsamrat27@gmail.com>
2025-01-06 00:58:22 +0545

    add control_flow.py with examples of comparison and logical operators

---
+++ control_flow.py
@@ -0,0 +1,14 @@
+#Comparision Operator
+
+print(1>2)
+print(2==2) 
+r="deuja"
+m="deuja"
+
+print(r==m)
+
+#logical Opertors
+# AND, OR
+print(1>2 and 2==2)
+print(3>2 and 2==2)
+print(3>2 and 2==1)

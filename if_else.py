9fd5b7c49689f7009e2c579c4aa4ad92cbb24eab
Samrat <paudelsamrat27@gmail.com>
2025-01-06 20:04:01 +0545

    add if_else.py with examples of if-elif-else control flow statements

---
+++ if_else.py
@@ -0,0 +1,14 @@
+p="abc"
+q="def"
+r=True
+
+if p==q:
+    print('matched')
+
+elif r:  
+    print("Okay") 
+
+else:
+    print("not matched") 
+
+#program terminates in the first true condition    

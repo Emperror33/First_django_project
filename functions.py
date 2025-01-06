afd9f304686ba8d5307eaf9f47784cca8745cc7e
Samrat <paudelsamrat27@gmail.com>
2025-01-06 20:03:54 +0545

    add functions.py with display_info and compare_num functions

---
+++ functions.py
@@ -0,0 +1,13 @@
+def display_info(name):
+    return f"Hello World {name}"
+r=display_info("Joseph")
+print(r) 
+
+def compare_num(num):
+    if num>10:
+        return print("Greater")
+    elif num<10:
+        return print("Lesser")
+    else:
+        return print( "Equal")
+compare_num(10)
